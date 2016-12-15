c1844eb193fbb22759ea95d58bee08ca65fd9e24
guttih <gudjonholm@gmail.com>
2016-12-15 15:29:22 +0000

    small script changes

--- scripts/native.bat
+++ scripts/native.bat
@@ -1,6 +1,19 @@
 @echo off
-call initnative.bat
+call native-init.bat
 echo.
+if "%1"=="h"     goto HELP
+if "%1"=="-h"    goto HELP
+if "%1"=="/h"    goto HELP
+if "%1"=="/?"    goto HELP
+if "%1"=="-help" goto HELP
+if "%1"=="/help" goto HELP
+goto BEGIN
+
+:HELP
+	echo Initializes the react-native bat file system.
+	echo. 
+	echo If no parameters are specified then all native bat commands are listed and a cd to project folder
+	goto ENDIR
 :BEGIN
   ::LIST avaialeble commands
   cd %NATIVE%
@@ -10,13 +23,12 @@ echo.
   echo.
   echo --------------------------------------------------------------------------------
   cd ..\projects
-  @if "%1x"=="x" goto dir
+  @if "%1x"=="x" goto DIR
     cd %1
     echo skrifa "code ." til að opna þetta project í Visual Studio Code
    
-:dir
+:DIR
 	dir
 	goto ENDIR
 
 :ENDIR
-::SET NATIVE=
