5eac7d8c1c1e938f4e86c4da8ce89d3b2784421e
gudjons <gudjons@advania.is>
2017-01-23 14:03:13 +0000

    stilun kodka

--- projects/about/src/common/About.js
+++ projects/about/src/common/About.js
@@ -5,28 +5,25 @@ import KeyValues from './KeyValues';
 class About extends Component {
 	render () {
 		let info = {
-			AppVersion: DeviceInfo.getReadableVersion(),
-			uniqueID : DeviceInfo.getUniqueID(),
-			instanceID : DeviceInfo.getInstanceID(),
-			deviceId : DeviceInfo.getDeviceId(),
-			manufacturer : DeviceInfo.getManufacturer(),
-			model : DeviceInfo.getModel(),
-			brand : DeviceInfo.getBrand(),
-			systemName : DeviceInfo.getSystemName(),
+			AppVersion    : DeviceInfo.getReadableVersion(),
+			uniqueID      : DeviceInfo.getUniqueID(),
+			instanceID    : DeviceInfo.getInstanceID(),
+			deviceId      : DeviceInfo.getDeviceId(),
+			manufacturer  : DeviceInfo.getManufacturer(),
+			model         : DeviceInfo.getModel(),
+			brand         : DeviceInfo.getBrand(),
+			systemName    : DeviceInfo.getSystemName(),
 			systemVersion : DeviceInfo.getSystemVersion(),
-			bundleId : DeviceInfo.getBundleId(),
-			readableVersion : DeviceInfo.getReadableVersion(),
-			deviceName : DeviceInfo.getDeviceName(),
-			userAgent : DeviceInfo.getUserAgent(),
-			deviceLocale : DeviceInfo.getDeviceLocale(),
+			bundleId      : DeviceInfo.getBundleId(),
+			deviceName    : DeviceInfo.getDeviceName(),
+			userAgent     : DeviceInfo.getUserAgent(),
+			deviceLocale  : DeviceInfo.getDeviceLocale(),
 			deviceCountry : DeviceInfo.getDeviceCountry(),
-			timezone : DeviceInfo.getTimezone(),
+			timezone      : DeviceInfo.getTimezone(),
 			//isEmulator : DeviceInfo.isEmulator(),
 			//isTablet : DeviceInfo.isTablet(),
 			//buildNumber : DeviceInfo.getBuildNumber(), //last      number  in getReadableVersion
 			//version : DeviceInfo.getVersion(),        // fyrst two numbers in getReadableVersion
-
-
 		};
 		if ( DeviceInfo.isEmulator() === true ) {
 			info.isEmulator = 'true';
