e627afd1e378c450dc22e4d3372e8b2b894b60ea
gudjons <gudjons@advania.is>
2017-01-24 14:31:06 +0000

    space between buttons

--- projects/about/src/common/Button.js
+++ projects/about/src/common/Button.js
@@ -23,14 +23,15 @@ const styles = {
 		paddingBottom: 12,
 	},
 	buttonStyle: {
-		flex: 1,
-		alignSelf: 'stretch',
+		//flex: 1,
+		//alignSelf: 'stretch',
 		backgroundColor: '#fff',
 		borderRadius: 5,
 		borderWidth: 1,
 		borderColor: '#007aff',
 		marginLeft: 5,
-		marginRight: 5
+		marginRight: 5,
+		marginBottom:10
 	}
 };
 
