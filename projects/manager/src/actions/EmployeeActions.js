9bc6b38dbd947f1686562f177010d28bd5e14442
gudjons <gudjons@advania.is>
2017-01-30 18:04:03 +0000

    villa í create employee

---
+++ projects/manager/src/actions/EmployeeActions.js
@@ -0,0 +1,11 @@
+//EmployeeActions
+import {
+	EMPLOYEE_UPDATE
+} from './types';
+
+export const employeeUpdate = ( { prop, value }) => {
+	return {
+		type: EMPLOYEE_UPDATE,
+		payload: { prop, value }
+	};
+};
---
+++ projects/manager/src/components/EmployeeList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+
+	render () {
+		return (
+			<View>
+				<Text> Employee List, item 1 </Text>
+				<Text> Employee List, item 2 </Text>
+				<Text> Employee List, item 3 </Text>
+				<Text> Employee List, item 4 </Text>
+				<Text> Employee List, item 5 </Text>
+				<Text> Employee List, item 6 </Text>
+			</View>
+		);
+	}
+}
+
+export default EmployeeList;
